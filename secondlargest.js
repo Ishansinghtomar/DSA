a4cdf59a731ae7c08d5d6e066ae99c1ef1ce7f54
Ishan Singh Tomar <ishan.tomar@indiamart.com>
2025-10-05 18:18:58 +0530

    slargeupdated

--- secondlargest.js
+++ secondlargest.js
@@ -11,3 +11,27 @@ function secondlargest(arr) {
 }
 let array = [78, 42, 11, 9, 0, 88, -7]
 console.log("Slargest"+secondlargest(array))
+
+/////////////////////////////////////////////////
+function slarge(arr)
+{
+  let lrg =-Infinity
+   let  slrg = -Infinity
+  for (let i = 0; i < arr.length; i++)
+  {
+    if (arr[i] > lrg)
+    {
+      slrg = lrg;
+      lrg=arr[i]
+    }
+    else if (arr[i]> slrg && (arr[i]!=lrg))
+    {
+      slrg=arr[i]
+
+    }
+  }
+  return slrg;
+}
+
+let arr1 = [7, 7, 8, 66, 66, 9, -2, -7]
+console.log(slarge(arr1));
